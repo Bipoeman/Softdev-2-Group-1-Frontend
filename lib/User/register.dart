1e430452abb63c11aa49c19380295355c91fa6a9
Bipoeman <75182220+Bipoeman@users.noreply.github.com>
2024-01-15 13:16:11 +0700

    re-structured

---
+++ lib/User/register.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+const Color backgroundColor = Color(0xffe8e8e8);
+const Color mainColor = Color(0xffd33333);
+
+TextFormField textField({
+  required String labelText,
+  required Color fillColor,
+  Icon? icon,
+  bool? obscureText,
+  TextInputType? inputType,
+}) {
+  return TextFormField(
+    keyboardType: inputType,
+    obscureText: obscureText??false,
+    decoration: InputDecoration(
+      fillColor: fillColor,
+      filled: true,
+      labelStyle: TextStyle(color: Colors.grey[400]),
+      contentPadding: const EdgeInsets.fromLTRB(30, 0, 5, 0),
+      labelText: labelText,
+      prefixIconColor: Colors.white,
+      prefixIcon: icon,
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(40),
+        borderSide: BorderSide.none,
+      ),
+    ),
+  );
+}
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: size.height * 0.2,
+          title: const Text(
+            "Create your account",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor: mainColor,
+          leading: GestureDetector(
+            child: const Icon(
+              Icons.arrow_back_ios,
+              color: Colors.white,
+            ),
+            onTap: () {
+              // Navigator.popAndPushNamed(context, "/login");
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(30),
+          child: CustomScrollView(
+            slivers: [
+              SliverFillRemaining(
+                hasScrollBody: false,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    textField(labelText: "Fullname", fillColor: backgroundColor),
+                    textField(labelText: "Email", fillColor: backgroundColor,inputType: TextInputType.emailAddress),
+                    textField(labelText: "Username", fillColor: backgroundColor),
+                    textField(labelText: "Password", fillColor: backgroundColor,obscureText: true),
+                    textField(labelText: "Repeat Password", fillColor: backgroundColor,obscureText: true),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:ruam_mitt/User/login.dart';
+import 'package:ruam_mitt/User/register.dart';
+
+
+ThemeData mainTheme = ThemeData(
+  colorScheme:
+      ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 203, 46, 35)),
+  useMaterial3: true,
+);
+
+// ThemeData customTheme = ThemeData(
+//   primaryTextTheme:
+// );
 
 void main() {
-  runApp(const MyApp());
+  runApp(const SuperApp());
+}
+
+class SuperApp extends StatefulWidget {
+  const SuperApp({super.key});
+
+  @override
+  State<SuperApp> createState() => _SuperAppState();
+}
+
+class _SuperAppState extends State<SuperApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: "/login",
+      routes: {
+        "/login": (context) => const LoginPage(),
+        "/register" : (context) => const RegisterPage()
+      },
+      title: "Super App Group 1",
+      theme: mainTheme,
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -12,25 +47,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: mainTheme,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
