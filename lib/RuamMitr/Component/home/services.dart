fba98a18125de78bf0449ac44a943ef098d3ed0b
SkyJTx <111367081+SkyJTx@users.noreply.github.com>
2024-02-02 21:30:16 +0700

    Add AppBox, [Contents, Services] Navigator on home page

---
+++ lib/RuamMitr/Component/home/services.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:ruam_mitt/RuamMitr/Component/home/app_box.dart';
+
+class ServicesWidget extends StatelessWidget {
+  const ServicesWidget({super.key});
+  final double size = 200;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Wrap(
+        spacing: 20,
+        runSpacing: 20,
+        children: [
+          AppBox(
+            appName: "Tuachuay",
+            width: size,
+            height: size,
+          ),
+          AppBox(
+            appName: "Market",
+            width: size,
+            height: size,
+          ),
+          AppBox(
+            appName: "Restroom",
+            width: size,
+            height: size,
+          ),
+          AppBox(
+            appName: "Bin",
+            width: size,
+            height: size,
+          ),
+          AppBox(
+            appName: "Dinodengzz",
+            width: size,
+            height: size,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/RuamMitr/home.dart
+++ lib/RuamMitr/home.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import 'package:ruam_mitt/RuamMitr/Component/avatar.dart';
 import 'package:ruam_mitt/RuamMitr/Component/search_box.dart';
+import 'package:ruam_mitt/RuamMitr/Component/home/services.dart';
 import "dart:math";
 
 class HomePage extends StatefulWidget {
@@ -31,65 +32,64 @@ class _HomePageState extends State<HomePage> {
           ),
           Container(
             margin: const EdgeInsets.all(32),
-            child: IntrinsicHeight(
-              child: Stack(
-                alignment: AlignmentDirectional.topCenter,
-                clipBehavior: Clip.none,
-                children: [
-                  Positioned(
-                    top: 35,
-                    width: [size.width * 0.8, 800.0].reduce(min),
-                    child: Container(
-                      padding: const EdgeInsets.fromLTRB(15, 40, 15, 15),
-                      decoration: BoxDecoration(
-                        color: theme.colorScheme.primaryContainer.withOpacity(0.8),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              TextButton(
-                                onPressed: () {
-                                  debugPrint("Contents");
-                                },
-                                child: Text(
-                                  "Contents",
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: theme.colorScheme.onBackground,
-                                    fontWeight: FontWeight.bold,
-                                  ),
+            child: Stack(
+              alignment: AlignmentDirectional.topCenter,
+              clipBehavior: Clip.none,
+              children: [
+                Positioned(
+                  top: 35,
+                  width: [size.width * 0.8, 800.0].reduce(min),
+                  child: Container(
+                    padding: const EdgeInsets.fromLTRB(15, 40, 15, 15),
+                    decoration: BoxDecoration(
+                      color: theme.colorScheme.primaryContainer.withOpacity(0.8),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                debugPrint("Contents");
+                              },
+                              child: Text(
+                                "Contents",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: theme.colorScheme.onBackground,
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
-                              TextButton(
-                                onPressed: () {
-                                  debugPrint("Services");
-                                },
-                                child: Text(
-                                  "Services",
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: theme.colorScheme.onBackground,
-                                    fontWeight: FontWeight.bold,
-                                  ),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                debugPrint("Services");
+                              },
+                              child: Text(
+                                "Services",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: theme.colorScheme.onBackground,
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
-                            ],
-                          ),
-                        ],
-                      ),
+                            ),
+                          ],
+                        ),
+                        const ServicesWidget(),
+                      ],
                     ),
                   ),
-                  Container(
-                    padding: const EdgeInsets.all(10),
-                    width: [300.0, size.width * 0.7].reduce(min),
-                    child: const SearchBox(),
-                  ),
-                ],
-              ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  width: [300.0, size.width * 0.7].reduce(min),
+                  child: const SearchBox(),
+                ),
+              ],
             ),
           ),
         ],
