ff4bad0125c0adf94be49c8155883410a4cbbfff
SkyJTx <111367081+SkyJTx@users.noreply.github.com>
2024-02-12 14:29:07 +0700

    Add PageRoute and BaseURL to global constant

---
+++ lib/global_const.dart
@@ -0,0 +1,9 @@
+const baseURL = "https://ruammitr.azurewebsites.net/";
+
+const loginPageRoute = "/login";
+const registerPageRoute = "/register";
+const ruamMitrPageRoute = {
+  "home": "/RuamMitr/home",
+  "profile": "/RuamMitr/profile",
+  "settings": "/RuamMitr/settings",
+};
