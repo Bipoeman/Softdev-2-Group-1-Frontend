3556fc896036151b140d678870758166dc103f28
Sahirann <s6501012630182@email.kmutnb.ac.th>
2024-02-16 04:01:31 +0700

    have navbar but cannot use

--- lib/Restroom/restroom.dart
+++ lib/Restroom/restroom.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:url_launcher/url_launcher.dart';
-
+import 'package:ruam_mitt/Restroom/Component/NavBar.dart';
+import "package:ruam_mitt/global_const.dart";
+import 'package:ruam_mitt/Restroom/Component/search_box.dart';
 class RestroomRover extends StatefulWidget {
   const RestroomRover({super.key});
 
@@ -11,14 +13,23 @@ class RestroomRover extends StatefulWidget {
 }
 
 class _RestroomRoverState extends State<RestroomRover> {
+  
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("P sud smart"),
-      ),
+      // appBar: AppBar(
+      //    flexibleSpace: Container(
+      //         padding: const EdgeInsets.only(top: 15),
+      //         height: 56,
+      //         width: size.width * 0.5,
+      //         child: const RestroomSearchBox(),
+      //       ),
+      // ),
+      // drawer: NavbarRestroomRover() ,
       body: Stack(
         children: [
+          
           FlutterMap(
             options: MapOptions(
               center: LatLng(13.825605, 100.514476),
@@ -59,17 +70,23 @@ class _RestroomRoverState extends State<RestroomRover> {
                 width: 130,
                 height: 130,
               )),
-          Positioned(
-              top: 20,
-              right: 5,
-              child: Image.asset(
-                "assets/images/RestroomRover/type.png",
-                width: 130,
-                height: 130,
-              ))
+           NavbarRestroomRover()
+          // Positioned(
+          //     child: NavbarRestroomRover(),
+          //     ),
+         
+          // Positioned(
+          //     top: 20,
+          //     right: 5,
+          //     child: Image.asset(
+          //       "assets/images/RestroomRover/type.png",
+          //       width: 130,
+          //       height: 130,
+          //     ))
               
         ],
       ),
+      
     );
   }
 }
