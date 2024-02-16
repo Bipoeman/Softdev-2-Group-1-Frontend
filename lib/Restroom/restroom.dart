f405642e88476f862060df0e2b9f90bfe5833451
Sahirann <s6501012630182@email.kmutnb.ac.th>
2024-02-16 23:45:49 +0700

    sidebar half

--- lib/Restroom/restroom.dart
+++ lib/Restroom/restroom.dart
@@ -9,6 +9,7 @@ import "package:ruam_mitt/global_const.dart";
 import 'package:ruam_mitt/Restroom/Component/search_box.dart';
 import 'package:ruam_mitt/Restroom/Component/map.dart';
 import 'package:ruam_mitt/Restroom/Component/navbarza.dart';
+
 class RestroomRover extends StatefulWidget {
   const RestroomRover({super.key});
 
@@ -17,23 +18,41 @@ class RestroomRover extends StatefulWidget {
 }
 
 class _RestroomRoverState extends State<RestroomRover> {
-  
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     // Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
+        key: _scaffoldKey, // กำหนด GlobalKey ให้กับ Scaffold
+        appBar: AppBar(
+          title : SizedBox(width: 300,child : RestroomSearchBox(),),
+          backgroundColor: Color(0xFFFFB703),
+          leading: IconButton(
+            icon: Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                shape: BoxShape.rectangle,
+                color: Colors.white,
+              ),
+              child: Center(
+                child: Icon(
+                  Icons.menu,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            onPressed: () {
+              _scaffoldKey.currentState
+                  ?.openDrawer(); // เรียกใช้งาน ScaffoldState เพื่อเปิด Drawer
+            },
+          ),
+        ),
+        drawer: Navbar2RestroomRover(),
+        body: MapRestroomRover(),
+        // drawerEdgeDragWidth: 500,
+        );
         
-        //  flexibleSpace: Container(
-        //       padding: const EdgeInsets.only(top: 15),
-        //       height: 56,
-        //       width: size.width * 0.5,
-        //       child: const RestroomSearchBox(),
-        //     ),
-      ),
-      drawer: Navbar2RestroomRover() ,
-      body: MapRestroomRover()
-      
-    );
   }
 }
