4135f73cd413e667cc25506987696e92d55d8521
KamitomeYuta <142527511+KamitomeYuta@users.noreply.github.com>
2024-02-17 23:40:19 +0700

    review slidebar
    Co-authored-by: Nutpapop Yasawut <winth03@users.noreply.github.com>

--- lib/Restroom/Review.dart
+++ lib/Restroom/Review.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:ruam_mitt/Restroom/Component/write_review.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:ruam_mitt/Restroom/Component/NavBar.dart';
 import "package:ruam_mitt/global_const.dart";
@@ -8,6 +9,7 @@ import 'package:ruam_mitt/Restroom/Component/search_box.dart';
 import 'package:ruam_mitt/Restroom/Component/map.dart';
 import 'package:ruam_mitt/Restroom/Component/comment.dart';
 import 'package:flutter_rating_native/flutter_rating_native.dart';
+import 'package:flutter_sliding_box/flutter_sliding_box.dart';
 import 'dart:math';
 
 class RestroomRoverReview extends StatefulWidget {
@@ -18,22 +20,24 @@ class RestroomRoverReview extends StatefulWidget {
 }
 
 class _RestroomRoverReviewState extends State<RestroomRoverReview> {
+  BoxController boxController = BoxController();
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     var rating = 2.5;
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromRGBO(255, 183, 3, 1),
+        backgroundColor: const Color.fromRGBO(255, 183, 3, 1),
         actions: <Widget>[
           Container(
             width: size.width,
             height: size.width * 0.2,
-            color: Color.fromRGBO(255, 183, 3, 1),
+            color: const Color.fromRGBO(255, 183, 3, 1),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Container(
+                SizedBox(
                   width: size.width * 0.3,
                   height: size.width * 0.2,
                   child: Image.network(
@@ -61,174 +65,198 @@ class _RestroomRoverReviewState extends State<RestroomRoverReview> {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: ConstrainedBox(
-          constraints: BoxConstraints(
-            minHeight: size.height -
-                [size.width * 0.4, 100.0].reduce(min) -
-                MediaQuery.of(context).padding.top,
-          ),
-          child: Container(
-            child: Center(
-              child: Container(
-                width: size.width,
-                height: size.height,
-                color: Color.fromRGBO(230, 230, 230, 1),
-                child: Column(
-                  children: [
-                    Container(
-                      height: size.height * 0.3,
-                      width: size.width * 0.8,
-                      padding: EdgeInsets.only(
-                        left: size.width * 0.01,
-                        top: size.height * 0.04,
-                        bottom: size.height * 0.01,
-                        right: size.width * 0.01,
-                      ),
-                      child: Image.network(
-                        "https://i.pinimg.com/564x/1c/13/1c/1c131cc30f7c203a4833b6983d025b03.jpg",
-                      ),
-                    ),
-                    Container(
-                      height: size.height * 0.07,
-                      width: size.width * 0.8,
-                      padding: EdgeInsets.only(
-                        top: size.height * 0.02,
-                      ),
-                      child: const Text(
-                        "Tai taeuk 81",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 25,
-                          fontWeight: FontWeight.w500,
+      body: SlidingBox(
+        controller: boxController,
+        physics: const NeverScrollableScrollPhysics(),
+        collapsed: true,
+        minHeight: 0,
+        maxHeight: 460,
+        draggable: false,
+        onBoxClose: () => FocusManager.instance.primaryFocus?.unfocus(),
+        body: ReviewSlideBar(cancelOnPressed: boxController.closeBox),
+        backdrop: Backdrop(
+          moving: false,
+          overlay: true,
+          body: SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: size.height -
+                    [size.width * 0.4, 100.0].reduce(min) -
+                    MediaQuery.of(context).padding.top,
+              ),
+              child: Center(
+                child: Container(
+                  width: size.width,
+                  height: size.height,
+                  color: const Color.fromRGBO(230, 230, 230, 1),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: size.height * 0.3,
+                        width: size.width * 0.8,
+                        padding: EdgeInsets.only(
+                          left: size.width * 0.01,
+                          top: size.height * 0.04,
+                          bottom: size.height * 0.01,
+                          right: size.width * 0.01,
+                        ),
+                        child: Image.network(
+                          "https://i.pinimg.com/564x/1c/13/1c/1c131cc30f7c203a4833b6983d025b03.jpg",
                         ),
                       ),
-                    ),
-                    Container(
-                      height: size.height * 0.002,
-                      width: size.width * 0.8,
-                      color: Color.fromRGBO(99, 99, 99, 1),
-                    ),
-                    Row(
-                      children: [
-                        Container(
-                          height: size.height * 0.09,
-                          width: size.width * 0.7,
-                          padding: EdgeInsets.only(
-                            left: size.width * 0.1,
-                            top: size.height * 0.01,
-                            bottom: size.height * 0.01,
-                          ),
-                          child: FlutterRating(
-                            rating: rating,
-                            size: size.height * 0.05,
-                            mainAxisAlignment: MainAxisAlignment.start,
+                      Container(
+                        height: size.height * 0.07,
+                        width: size.width * 0.8,
+                        padding: EdgeInsets.only(
+                          top: size.height * 0.02,
+                        ),
+                        child: const Text(
+                          "Tai taeuk 81",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w500,
                           ),
                         ),
-                        Container(
-                          height: size.height * 0.09,
-                          width: size.width * 0.2,
-                          padding: EdgeInsets.only(
-                            left: size.width * 0.07,
-                            top: size.height * 0.027,
-                            bottom: size.height * 0.01,
-                          ),
-                          child: Text(
-                            rating.toString(),
-                            style: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 25,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      height: size.height * 0.12,
-                      width: size.width * 0.7,
-                      padding: EdgeInsets.only(
-                        bottom: size.height * 0.01,
                       ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      Container(
+                        height: size.height * 0.002,
+                        width: size.width * 0.8,
+                        color: const Color.fromRGBO(99, 99, 99, 1),
+                      ),
+                      Row(
                         children: [
                           Container(
-                            height: size.height * 0.06,
-                            width: size.width * 0.25,
-                            decoration: BoxDecoration(
-                              color: Color.fromRGBO(255, 183, 3, 1),
-                              borderRadius: BorderRadius.circular(20),
+                            height: size.height * 0.09,
+                            width: size.width * 0.7,
+                            padding: EdgeInsets.only(
+                              left: size.width * 0.1,
+                              top: size.height * 0.01,
+                              bottom: size.height * 0.01,
                             ),
-                            child: ElevatedButton(
-                              onPressed: () {},
-                              style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStateProperty.all<Color>(
-                                  Color.fromRGBO(255, 183, 3, 1),
-                                ),
-                                shape:
-                                    MaterialStateProperty.all<OutlinedBorder>(
-                                  RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                ),
-                              ),
-                              child: const Icon(
-                                Icons.reviews,
-                                color: Colors.black,
-                                size: 30,
-                              ),
+                            child: FlutterRating(
+                              rating: rating,
+                              size: size.height * 0.05,
+                              mainAxisAlignment: MainAxisAlignment.start,
                             ),
                           ),
                           Container(
-                            height: size.height * 0.06,
-                            width: size.width * 0.25,
-                            decoration: BoxDecoration(
-                              color: Color.fromRGBO(255, 183, 3, 1),
-                              borderRadius: BorderRadius.circular(20),
+                            height: size.height * 0.09,
+                            width: size.width * 0.2,
+                            padding: EdgeInsets.only(
+                              left: size.width * 0.07,
+                              top: size.height * 0.027,
+                              bottom: size.height * 0.01,
                             ),
-                            child: ElevatedButton(
-                              onPressed: () {},
-                              style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStateProperty.all<Color>(
-                                  Color.fromRGBO(255, 183, 3, 1),
-                                ),
-                                shape:
-                                    MaterialStateProperty.all<OutlinedBorder>(
-                                  RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                ),
-                              ),
-                              child: const Icon(
-                                Icons.report,
+                            child: Text(
+                              rating.toString(),
+                              style: const TextStyle(
                                 color: Colors.black,
-                                size: 30,
+                                fontSize: 25,
+                                fontWeight: FontWeight.w500,
                               ),
                             ),
                           ),
                         ],
                       ),
-                    ),
-                    Container(
-                      height: size.height * 0.002,
-                      width: size.width * 0.8,
-                      color: Color.fromRGBO(99, 99, 99, 1),
-                    ),
-                    Expanded(
-                      child: ListView.builder(
-                        itemCount: 10, // จำนวนรายการที่ต้องการแสดง
-                        itemBuilder: (context, index) {
-                          return SizedBox(
-                            height: 250, // ระบุความสูงที่ต้องการ
-                            child: Cardcomment(),
-                          );
-                        },
+                      Container(
+                        height: size.height * 0.12,
+                        width: size.width * 0.7,
+                        padding: EdgeInsets.only(
+                          bottom: size.height * 0.01,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              height: size.height * 0.06,
+                              width: size.width * 0.25,
+                              decoration: BoxDecoration(
+                                color: const Color.fromRGBO(255, 183, 3, 1),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  // showSlidingBox(
+                                  //   context: context,
+                                  //   box: SlidingBox(
+                                  //     draggable: false,
+                                  //     maxHeight: maxBoxHeight,
+                                  //     body: ReviewSlideBar(
+                                  //       setHeight: setMaxBoxHeight,
+                                  //     ),
+                                  //   ),
+                                  // );
+                                  boxController.openBox();
+                                },
+                                style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                    const Color.fromRGBO(255, 183, 3, 1),
+                                  ),
+                                  shape:
+                                      MaterialStateProperty.all<OutlinedBorder>(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  ),
+                                ),
+                                child: const Icon(
+                                  Icons.reviews,
+                                  color: Colors.black,
+                                  size: 30,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              height: size.height * 0.06,
+                              width: size.width * 0.25,
+                              decoration: BoxDecoration(
+                                color: const Color.fromRGBO(255, 183, 3, 1),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                    const Color.fromRGBO(255, 183, 3, 1),
+                                  ),
+                                  shape:
+                                      MaterialStateProperty.all<OutlinedBorder>(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  ),
+                                ),
+                                child: const Icon(
+                                  Icons.report,
+                                  color: Colors.black,
+                                  size: 30,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                      Container(
+                        height: size.height * 0.002,
+                        width: size.width * 0.8,
+                        color: const Color.fromRGBO(99, 99, 99, 1),
+                      ),
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: 10, // จำนวนรายการที่ต้องการแสดง
+                          itemBuilder: (context, index) {
+                            return const SizedBox(
+                              height: 250, // ระบุความสูงที่ต้องการ
+                              child: Cardcomment(),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
