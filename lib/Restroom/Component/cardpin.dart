e666c637661e7b1ac10d0fb8719b6e61723cde36
pumNicharee <s6501012610041@email.kmutnb.ac.th>
2024-02-17 20:17:37 +0700

    cardpin update

--- lib/Restroom/Component/cardpin.dart
+++ lib/Restroom/Component/cardpin.dart
@@ -12,177 +12,165 @@ class _CardpinState extends State<Cardpin> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: Center(
-        child: Container(
-          height: size.height * 0.55,
-          width: size.width * 0.8,
-          decoration: BoxDecoration(
-            color: Color.fromARGB(255, 255, 255, 255),
-            borderRadius: BorderRadius.circular(35),
+    return Container(
+      height: 300,
+      width: 300,
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 255, 255, 255),
+        borderRadius: BorderRadius.circular(35),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+              height: 40,
+              width: 250,
+              padding: const EdgeInsets.only(
+                left: 35,
+                top: 10,
+              ),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Bally",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 22,
+                        fontWeight: FontWeight.w600,
+                      )),
+                  Icon(
+                    Icons.woman_2,
+                    size: 35,
+                  )
+                ],
+              )),
+          Container(
+            height: 25,
+            width: 320,
+            padding: const EdgeInsets.only(
+              left: 35,
+              top: 4,
+            ),
+            child: const Text("97/2 บางซ่อน",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400,
+                )),
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          Row(
             children: [
               Container(
-                height: size.height * 0.09,
-                width: size.width * 0.7,
-                // color: Color.fromARGB(255, 255, 255, 255),
-                padding: EdgeInsets.only(
-                  left: size.width * 0.1,
-                  top: size.height * 0.03,
-                  bottom: size.height * 0.01,
-                  right: size.width * 0.1,
+                height: 20,
+                width: 60,
+                padding: const EdgeInsets.only(
+                  left: 35,
+                  top: 0.8,
                 ),
-                child: const Text("Bally",
+                child: const Text("5.0",
                     style: TextStyle(
                       color: Colors.black,
-                      fontSize: 27,
-                      fontWeight: FontWeight.w600,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500,
                     )),
               ),
               Container(
-                height: size.height * 0.05,
-                width: size.width * 0.7,
-                padding: EdgeInsets.only(
-                  left: size.width * 0.1,
-                  top: size.height * 0.001,
-                  bottom: size.height * 0.01,
-                  right: size.width * 0.1,
+                height:20,
+                width: 160,
+                child: const FlutterRating(
+                  rating: 3.5,
+                  size: 20,
+                  mainAxisAlignment: MainAxisAlignment.center,
                 ),
-                child: const Text("97/2 บางซ่อน",
+              ),
+              // ),
+              Container(
+                height: 20,
+                width: 40,
+                padding: const EdgeInsets.only(
+                  top: 0.8,
+                ),
+                child: const Text("[ 9 ]",
                     style: TextStyle(
                       color: Colors.black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w400,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500,
                     )),
               ),
-              Row(
-                children: [
-                  Container(
-                    height: size.height * 0.05,
-                    width: size.width * 0.2,
-                    padding: EdgeInsets.only(
-                      left: size.width * 0.1,
-                      top: size.height * 0.01,
-                    ),
-                    child: const Text("5.0",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 17,
-                          fontWeight: FontWeight.w500,
-                        )),
-                  ),
-                  Container(
-                    height: size.height * 0.05,
-                    width: size.width * 0.35,
-                    padding: EdgeInsets.only(
-                      left: size.width * 0.01,
-                      top: size.height * 0.01,
-                      bottom: size.height * 0.01,
-                      right: size.width * 0.01,
-                    ),
-                    child: FlutterRating(
-                      rating: 3.5,
-                      size: size.height * 0.03,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                    ),
-                  ),
-                  // ),
-                  Container(
-                    height: size.height * 0.05,
-                    width: size.width * 0.15,
-                    padding: EdgeInsets.only(
-                      left: size.width * 0.01,
-                      top: size.height * 0.01,
-                    ),
-                    child: const Text("[ 9 ]",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 17,
-                          fontWeight: FontWeight.w500,
-                        )),
-                  ),
-                ],
-              ),
-              Container(
-                  height: size.height * 0.21,
-                  width: size.width * 0.8,
-                  padding: EdgeInsets.only(
-                    left: size.width * 0.1,
-                    top: size.height * 0.01,
-                    bottom: size.height * 0.01,
-                    right: size.width * 0.1,
-                  ),
-                  child: Image.network(
-                      "https://i.pinimg.com/564x/97/15/0f/97150f3cc7e93677495133ffe6ea77c3.jpg")),
-              Container(
-                  height: size.height * 0.1,
-                  width: size.width * 0.8,
-                  padding: EdgeInsets.only(
-                    left: size.width * 0.1,
-                    top: size.height * 0.01,
-                    bottom: size.height * 0.01,
-                    right: size.width * 0.1,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        height: size.height * 0.06,
-                        width: size.width * 0.25,
-                        decoration: BoxDecoration(
-                          color: Color.fromRGBO(255, 183, 3, 1),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all<Color>(
-                                Color.fromRGBO(255, 183, 3, 1)),
-                            shape: MaterialStateProperty.all<OutlinedBorder>(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                            ),
-                          ),
-                          child: const Icon(
-                            Icons.directions,
-                            color: Colors.black,
-                            size: 30,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: size.height * 0.06,
-                        width: size.width * 0.25,
-                        decoration: BoxDecoration(
-                          color: Color.fromRGBO(255, 183, 3, 1),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all<Color>(
-                                Color.fromRGBO(255, 183, 3, 1)),
-                            shape: MaterialStateProperty.all<OutlinedBorder>(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                            ),
-                          ),
-                          child: const Icon(
-                            Icons.reviews,
-                            color: Colors.black,
-                            size: 30,
-                          ),
-                        ),
-                      ),
-                    ],
-                  )),
             ],
           ),
-        ),
+          Container(
+              height: 150,
+              width: 300,
+              padding: const EdgeInsets.only(
+                left: 1,
+              ),
+              child: Image.network(
+                  "https://i.pinimg.com/564x/97/15/0f/97150f3cc7e93677495133ffe6ea77c3.jpg")),
+          Container(
+              height: 55,
+              width: 300,
+              padding: const EdgeInsets.only(
+                left: 35,
+                top: 2,
+                right: 35,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: 45,
+                    width: 90,
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(255, 183, 3, 1),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            Color.fromRGBO(255, 183, 3, 1)),
+                        shape: MaterialStateProperty.all<OutlinedBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      child: const Icon(
+                        Icons.directions,
+                        color: Colors.black,
+                        size: 30,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 45,
+                    width: 90,
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(255, 183, 3, 1),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            Color.fromRGBO(255, 183, 3, 1)),
+                        shape: MaterialStateProperty.all<OutlinedBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      child: const Icon(
+                        Icons.reviews,
+                        color: Colors.black,
+                        size: 30,
+                      ),
+                    ),
+                  ),
+                ],
+              )),
+        ],
       ),
     );
   }
