c5e632e2062af822eec13761be0c93a9fd56dd10
pumNicharee <s6501012610041@email.kmutnb.ac.th>
2024-02-19 19:56:00 +0700

    blogger page complete

--- lib/TuachuayDekhor/Component/navbar.dart
+++ lib/TuachuayDekhor/Component/navbar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ruam_mitt/TuachuayDekhor/Component/search_box.dart';
 import 'package:ruam_mitt/global_const.dart';
 import 'package:ruam_mitt/global_var.dart';
+// import 'package:ruam_mitt/RuamMitr/Component/home_v2/central_v2.dart';
 
 class NavbarTuachuayDekhor extends StatelessWidget {
   const NavbarTuachuayDekhor({
