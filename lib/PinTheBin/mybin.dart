03426384f1de3674acad2111bd9a36e43f744029
Nuttawon <s6501012630107@email.kmutnb.ac.th>
2024-02-21 23:23:36 +0700

    add scrollview

--- lib/PinTheBin/mybin.dart
+++ lib/PinTheBin/mybin.dart
@@ -1,5 +1,6 @@
 import "dart:convert";
 import "package:flutter/material.dart";
+import "package:flutter_sliding_box/flutter_sliding_box.dart";
 import "package:http/http.dart" as http;
 import "package:provider/provider.dart";
 import "package:ruam_mitt/PinTheBin/navbar.dart";
@@ -16,16 +17,76 @@ class MyBinPage extends StatefulWidget {
 
 class _MyBinState extends State<MyBinPage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-  dynamic binData = {
-    [
-      {"latitude": 13.825605, "longitude": 100.514476, "description": "test"},
-      {"latitude": 13.8256, "longitude": 100.5144, "description": "test"},
-      {"latitude": 13.82, "longitude": 100.51, "description": "test"},
-      {"latitude": 13, "longitude": 100, "description": "test"},
-    ]
-  };
+  dynamic binData = [
+    {
+      "id": "1",
+      "location": "Street X, City Y",
+      "description": "XY",
+      "latitude": "50",
+      "longitude": "50",
+      "bintype": {
+        "redbin": false,
+        "greenbin": false,
+        "yellow": false,
+        "bluebin": false
+      }
+    },
+    {
+      "id": "2",
+      "location": "Street W, City Y",
+      "description": "WY",
+      "latitude": "50",
+      "longitude": "40",
+      "bintype": {
+        "redbin": false,
+        "greenbin": false,
+        "yellow": false,
+        "bluebin": false
+      }
+    },
+    {
+      "id": "3",
+      "location": "Street W, City Z",
+      "description": "WZ",
+      "latitude": "40",
+      "longitude": "50",
+      "bintype": {
+        "redbin": true,
+        "greenbin": true,
+        "yellow": true,
+        "bluebin": false
+      }
+    },
+    {
+      "id": "4",
+      "location": "Street W, City Y",
+      "description": "WY",
+      "latitude": "50",
+      "longitude": "40",
+      "bintype": {
+        "redbin": false,
+        "greenbin": false,
+        "yellow": false,
+        "bluebin": false
+      }
+    },
+    {
+      "id": "5",
+      "location": "Street W, City Z",
+      "description": "WZ",
+      "latitude": "40",
+      "longitude": "50",
+      "bintype": {
+        "redbin": true,
+        "greenbin": true,
+        "yellow": true,
+        "bluebin": false
+      }
+    }
+  ];
+
   // Future<http.Response> myBinInfo() async {
-  //   Uri url = Uri.parse("$api$pinTheBinGetBinRoute");
+  //   Uri url = Uri.parse("$api$pinTheBinMyBinRoute");
   //   http.Response res = await http.get(url, headers: {"Authorization": ""});
   //   print(res.body);
   //   return res;
@@ -49,7 +110,7 @@ class _MyBinState extends State<MyBinPage> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-
+    // BoxController binInfoController = BoxController();
     ThemeProvider themes = Provider.of<ThemeProvider>(context);
     ThemeData pinTheBinTheme = themes.themeFrom("PinTheBin")!.themeData;
     return Theme(
@@ -104,17 +165,46 @@ class _MyBinState extends State<MyBinPage> {
                           ))
                     ]
                   : [
-                      Center(
-                          heightFactor: size.height * 0.01,
-                          child: Text(
-                            'No Bin Found!',
-                            style: GoogleFonts.getFont(
-                              'Sen',
-                              color: Color(0xFFF77F00),
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ))
+                      Expanded(
+                          child: Padding(
+                        padding: EdgeInsets.only(
+                          top: size.height * 0.1,
+                          left: size.width * 0.05,
+                        ),
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: binData.map<Widget>((data) {
+                              return Container(
+                                width: size.width * 0.9,
+                                height: size.height * 0.2,
+                                margin: EdgeInsets.symmetric(vertical: 5.0),
+                                padding: EdgeInsets.all(10.0),
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.grey),
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Bin Name: ${data["location"]}',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    SizedBox(height: 5),
+                                    Text('Description: ${data["description"]}'),
+                                    Text('Latitude: ${data["latitude"]}'),
+                                    Text('Longitude: ${data["longitude"]}'),
+                                    Text(
+                                      'Type: ${data["bintype"]["redbin"] ? "Red Bin" : ""} ${data["bintype"]["greenbin"] ? "Green Bin" : ""} ${data["bintype"]["yellow"] ? "Yellow Bin" : ""}',
+                                    ),
+                                  ],
+                                ),
+                              );
+                            }).toList(),
+                          ),
+                        ),
+                      ))
                     ],
             )
           ]),
