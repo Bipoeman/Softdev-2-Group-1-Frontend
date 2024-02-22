fe8d5ee3cfed3722473d677396956cf414e1cdb6
Nuttawon <s6501012630107@email.kmutnb.ac.th>
2024-02-22 22:47:09 +0700

    link to backend

--- lib/PinTheBin/mybin.dart
+++ lib/PinTheBin/mybin.dart
@@ -1,12 +1,13 @@
 import "dart:convert";
 import "package:flutter/material.dart";
-import "package:flutter_sliding_box/flutter_sliding_box.dart";
+// import "package:flutter_sliding_box/flutter_sliding_box.dart";
 import "package:http/http.dart" as http;
 import "package:provider/provider.dart";
 import "package:ruam_mitt/PinTheBin/navbar.dart";
 import "package:ruam_mitt/RuamMitr/Component/theme.dart";
 import "package:ruam_mitt/global_const.dart";
 import 'package:google_fonts/google_fonts.dart';
+import 'package:ruam_mitt/global_var.dart';
 
 class MyBinPage extends StatefulWidget {
   const MyBinPage({super.key});
@@ -17,95 +18,30 @@ class MyBinPage extends StatefulWidget {
 
 class _MyBinState extends State<MyBinPage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-  dynamic binData = [
-    {
-      "id": "1",
-      "location": "Street X, City Y",
-      "description": "XY",
-      "latitude": "50",
-      "longitude": "50",
-      "bintype": {
-        "redbin": false,
-        "greenbin": false,
-        "yellow": false,
-        "bluebin": false
-      }
-    },
-    {
-      "id": "2",
-      "location": "Street W, City Y",
-      "description": "WY",
-      "latitude": "50",
-      "longitude": "40",
-      "bintype": {
-        "redbin": false,
-        "greenbin": false,
-        "yellow": false,
-        "bluebin": false
-      }
-    },
-    {
-      "id": "3",
-      "location": "Street W, City Z",
-      "description": "WZ",
-      "latitude": "40",
-      "longitude": "50",
-      "bintype": {
-        "redbin": true,
-        "greenbin": true,
-        "yellow": true,
-        "bluebin": false
-      }
-    },
-    {
-      "id": "4",
-      "location": "Street W, City Y",
-      "description": "WY",
-      "latitude": "50",
-      "longitude": "40",
-      "bintype": {
-        "redbin": false,
-        "greenbin": false,
-        "yellow": false,
-        "bluebin": false
-      }
-    },
-    {
-      "id": "5",
-      "location": "Street W, City Z",
-      "description": "WZ",
-      "latitude": "40",
-      "longitude": "50",
-      "bintype": {
-        "redbin": true,
-        "greenbin": true,
-        "yellow": true,
-        "bluebin": false
-      }
-    }
-  ];
+  dynamic binData = [];
 
-  // Future<http.Response> myBinInfo() async {
-  //   Uri url = Uri.parse("$api$pinTheBinMyBinRoute");
-  //   http.Response res = await http.get(url, headers: {"Authorization": ""});
-  //   print(res.body);
-  //   return res;
-  // }
+  Future<http.Response> myBinInfo() async {
+    Uri url = Uri.parse("$api$pinTheBinMyBinRoute");
+    http.Response res =
+        await http.get(url, headers: {"Authorization": "Bearer $publicToken"});
+    print(res.body);
+    return res;
+  }
 
-  // @override
-  // void initState() {
-  //   // TODO: implement initState
-  //   super.initState();
-  //   print("Init Bin Page");
-  //   myBinInfo().then((response) {
-  //     // print("Response");
-  //     // print(response.body);
-  //     setState(() {
-  //       binData = jsonDecode(response.body);
-  //     });
-  //     // print(binData);
-  //   });
-  // }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print("Init Bin Page");
+    myBinInfo().then((response) {
+      // print("Response");
+      // print(response.body);
+      setState(() {
+        binData = jsonDecode(response.body);
+      });
+      // print(binData);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -175,32 +111,84 @@ class _MyBinState extends State<MyBinPage> {
                           child: Column(
                             children: binData.map<Widget>((data) {
                               return Container(
-                                width: size.width * 0.9,
-                                height: size.height * 0.2,
-                                margin: EdgeInsets.symmetric(vertical: 5.0),
-                                padding: EdgeInsets.all(10.0),
-                                decoration: BoxDecoration(
-                                  border: Border.all(color: Colors.grey),
-                                  borderRadius: BorderRadius.circular(10.0),
-                                ),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      'Bin Name: ${data["location"]}',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                    SizedBox(height: 5),
-                                    Text('Description: ${data["description"]}'),
-                                    Text('Latitude: ${data["latitude"]}'),
-                                    Text('Longitude: ${data["longitude"]}'),
-                                    Text(
-                                      'Type: ${data["bintype"]["redbin"] ? "Red Bin" : ""} ${data["bintype"]["greenbin"] ? "Green Bin" : ""} ${data["bintype"]["yellow"] ? "Yellow Bin" : ""}',
-                                    ),
-                                  ],
-                                ),
-                              );
+                                  width: size.width * 0.9,
+                                  height: size.height * 0.3,
+                                  margin: EdgeInsets.symmetric(vertical: 5.0),
+                                  padding: EdgeInsets.all(10.0),
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.grey),
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                  child: Stack(
+                                    children: [
+                                      Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            'Bin Name: ${data["location"]}',
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          SizedBox(height: 5),
+                                          Text(
+                                            'Type: ${data["bintype"]["redbin"] ? "\nDanger" : ""} ${data["bintype"]["greenbin"] ? "\nWaste" : ""} ${data["bintype"]["yellow"] ? "\nRecycle" : ""} ${data["bintype"]["bluebin"] ? "\nGeneral" : ""}',
+                                          ),
+                                          Row(
+                                            // verticalDirection:
+                                            //     VerticalDirection.down,
+                                            children: [
+                                              ElevatedButton.icon(
+                                                icon: Icon(
+                                                  Icons.edit,
+                                                  color: const Color.fromARGB(
+                                                      255, 0, 0, 0),
+                                                  size: 30.0,
+                                                ),
+                                                label: Text('Edit'),
+                                                onPressed: () {
+                                                  Navigator.pushNamed(
+                                                    context,
+                                                    pinthebinPageRoute[
+                                                        "editbin"]!,
+                                                    arguments: {
+                                                      'Bininfo':
+                                                          '${data["id"]}',
+                                                    },
+                                                  );
+                                                },
+                                              ),
+                                              const SizedBox(width: 10),
+                                              ElevatedButton.icon(
+                                                icon: const Icon(
+                                                  Icons.delete,
+                                                  color: Color.fromARGB(
+                                                      255, 0, 0, 0),
+                                                  size: 30.0,
+                                                ),
+                                                label: Text('Delete'),
+                                                onPressed: () {
+                                                  print('Delete');
+                                                },
+                                              ),
+                                            ],
+                                          )
+                                        ],
+                                      ),
+                                      Container(
+                                        padding: EdgeInsets.only(
+                                            left: size.width * 0.6),
+                                        child: data["picture"] == null
+                                            ? Image.asset(
+                                                "assets/images/PinTheBin/bin_null.png",
+                                                fit: BoxFit.cover,
+                                                width: size.width * 0.2,
+                                                height: size.width * 0.2,
+                                              )
+                                            : Image.network(data["picture"]),
+                                      )
+                                    ],
+                                  ));
                             }).toList(),
                           ),
                         ),
