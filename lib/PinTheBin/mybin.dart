59c37e533b9350c22686d194dcb48d31c4f1689b
Nuttawon <s6501012630107@email.kmutnb.ac.th>
2024-02-23 23:30:13 +0700

    change header, slidebar and decorate page

--- lib/PinTheBin/mybin.dart
+++ lib/PinTheBin/mybin.dart
@@ -1,14 +1,12 @@
 import "dart:convert";
 import "package:flutter/material.dart";
-// import "package:flutter_sliding_box/flutter_sliding_box.dart";
 import "package:http/http.dart" as http;
-import "package:provider/provider.dart";
+import "package:latlong2/latlong.dart";
 import "package:ruam_mitt/PinTheBin/bin_drawer.dart";
-import "package:ruam_mitt/PinTheBin/navbar.dart";
-import "package:ruam_mitt/RuamMitr/Component/theme.dart";
 import "package:ruam_mitt/global_const.dart";
 import 'package:google_fonts/google_fonts.dart';
 import 'package:ruam_mitt/global_var.dart';
+import 'package:ruam_mitt/PinTheBin/home.dart';
 
 class MyBinPage extends StatefulWidget {
   const MyBinPage({super.key});
@@ -20,7 +18,9 @@ class MyBinPage extends StatefulWidget {
 class _MyBinState extends State<MyBinPage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   dynamic binData = [];
-
+  SearchController searchBinController = SearchController();
+  FocusNode focusNode = FocusNode();
+  LatLng? centerMark;
   Future<http.Response> myBinInfo() async {
     Uri url = Uri.parse("$api$pinTheBinMyBinRoute");
     http.Response res =
@@ -35,8 +35,8 @@ class _MyBinState extends State<MyBinPage> {
     super.initState();
     print("Init Bin Page");
     myBinInfo().then((response) {
-      // print("Response");
-      // print(response.body);
+      print("Response");
+      print(response.body);
       setState(() {
         binData = jsonDecode(response.body);
       });
@@ -47,42 +47,78 @@ class _MyBinState extends State<MyBinPage> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    // BoxController binInfoController = BoxController();
-    ThemeProvider themes = Provider.of<ThemeProvider>(context);
-    ThemeData pinTheBinTheme = themes.themeFrom("PinTheBin")!.themeData;
-    return Theme(
-      data: pinTheBinTheme,
-      child: Scaffold(
-        key: _scaffoldKey,
-        body: SafeArea(
-          child: Stack(children: [
-            Positioned(
-              top: size.height * 0.01,
-              left: size.width * 0.025,
-              child: GestureDetector(
-                onTap: () {
-                  _scaffoldKey.currentState?.openDrawer();
-                },
-                child: Container(
-                  width: size.width * 0.1,
-                  height: size.height * 0.05,
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFF77F00),
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: const Icon(Icons.menu),
+    // ThemeProvider themes = Provider.of<ThemeProvider>(context);
+    // ThemeData pinTheBinTheme = themes.themeFrom("PinTheBin")!.themeData;
+    return Scrollbar(
+      // thumbVisibility: true,
+      thickness: 10,
+      trackVisibility: true,
+      radius: Radius.circular(10),
+      child: Theme(
+        data: ThemeData(
+          fontFamily: "Sen",
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color(0xFFF9957F),
+            background: const Color(0xFFFFFFFF),
+          ),
+          textTheme: TextTheme(
+            headlineMedium: const TextStyle(
+              fontSize: 35,
+              fontWeight: FontWeight.w800,
+              color: Colors.white,
+            ),
+            headlineSmall: TextStyle(
+              fontSize: 30,
+              overflow: TextOverflow.fade,
+              fontWeight: FontWeight.w800,
+              color: const Color(0xFF003049),
+              shadows: [
+                Shadow(
+                  blurRadius: 20,
+                  offset: const Offset(0, 3),
+                  color: const Color(0xFF003049).withOpacity(0.3),
                 ),
+              ],
+            ),
+            displayMedium: TextStyle(
+              fontSize: 20,
+              overflow: TextOverflow.fade,
+              fontWeight: FontWeight.normal,
+              color: const Color(0xFF003049).withOpacity(0.69),
+            ),
+          ),
+          appBarTheme: const AppBarTheme(
+            iconTheme: IconThemeData(
+              color: Colors.white,
+              size: 35,
+            ),
+          ),
+          drawerTheme: const DrawerThemeData(
+            scrimColor: Colors.transparent,
+            backgroundColor: Color(0xFFF9957F),
+          ),
+          searchBarTheme: SearchBarThemeData(
+            textStyle: MaterialStatePropertyAll(
+              TextStyle(
+                fontFamily: GoogleFonts.getFont("Inter").fontFamily,
+                color: Colors.black,
               ),
             ),
+          ),
+        ),
+        child: Scaffold(
+          key: _scaffoldKey,
+          body: Stack(children: [
             Container(
-              padding: EdgeInsets.fromLTRB(
-                  size.width * 0.36, size.height * 0.01, 0, 0),
-              child: Text(
-                "My Bin",
-                style: GoogleFonts.getFont(
-                  'Sen',
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  colors: [
+                    Color(0xFFF9957F),
+                    Color(0xFFE69FA1),
+                    Color(0xFFF3F6D1),
+                  ],
                 ),
               ),
             ),
@@ -103,12 +139,13 @@ class _MyBinState extends State<MyBinPage> {
                     ]
                   : [
                       Expanded(
-                          child: Padding(
-                        padding: EdgeInsets.only(
-                          top: size.height * 0.1,
-                          left: size.width * 0.05,
-                        ),
-                        child: SingleChildScrollView(
+                          child: SingleChildScrollView(
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                            top: size.height * 0.23,
+                            left: size.width * 0.05,
+                            bottom: 20,
+                          ),
                           child: Column(
                             children: binData.map<Widget>((data) {
                               return Container(
@@ -117,9 +154,16 @@ class _MyBinState extends State<MyBinPage> {
                                   margin: EdgeInsets.symmetric(vertical: 5.0),
                                   padding: EdgeInsets.all(10.0),
                                   decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    border: Border.all(color: Colors.grey),
-                                    borderRadius: BorderRadius.circular(10.0),
+                                    color: Color.fromRGBO(255, 255, 255, 0.6),
+                                    borderRadius: BorderRadius.circular(25.0),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Color.fromRGBO(
+                                            126, 120, 120, 0.247),
+                                        offset: Offset(0, 4),
+                                        blurRadius: 4,
+                                      ),
+                                    ],
                                   ),
                                   child: Stack(
                                     children: [
@@ -181,7 +225,7 @@ class _MyBinState extends State<MyBinPage> {
                                                   pinthebinPageRoute[
                                                       "editbin"]!,
                                                   arguments: {
-                                                    'Bininfo': '${data["id"]}',
+                                                    'Bininfo': '${data}',
                                                   },
                                                 );
                                               },
@@ -220,13 +264,97 @@ class _MyBinState extends State<MyBinPage> {
                             }).toList(),
                           ),
                         ),
-                      ))
+                      )),
                     ],
-            )
+            ),
+            MyPinTheBinAppBar(scaffoldKey: _scaffoldKey),
+            PinTheBinSearchBar(
+              size: size,
+              searchAnchorController: searchBinController,
+              binDataList: binData,
+              focusNode: focusNode,
+              parentKey: widget.key,
+              onSelected: (selectedValue) {
+                // print("Selected $selectedValue");
+                binData.forEach((eachBin) {
+                  if (eachBin['location'] == selectedValue) {
+                    print("Pin the bin");
+
+                    setState(() {
+                      focusNode.unfocus();
+                      // centerMark =
+                      //     LatLng(eachBin['latitude'], eachBin['longitude']);
+                      // mapController.move(centerMark!, 15);
+                    });
+                  }
+                });
+              },
+            ),
           ]),
+          drawerScrimColor: Colors.transparent,
+          drawer: const BinDrawer(),
         ),
-        drawerScrimColor: Colors.transparent,
-        drawer: const NavBar(),
+      ),
+    );
+  }
+}
+
+class MyPinTheBinAppBar extends StatelessWidget {
+  const MyPinTheBinAppBar({
+    super.key,
+    required GlobalKey<ScaffoldState> scaffoldKey,
+  }) : _scaffoldKey = scaffoldKey;
+
+  final GlobalKey<ScaffoldState> _scaffoldKey;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 130,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(30), bottomRight: Radius.circular(30)),
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: <Color>[
+            Color(0xFFF99680),
+            Color(0xFFF8A88F),
+          ],
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              const SizedBox(width: 20),
+              GestureDetector(
+                child: Icon(
+                  Icons.menu_rounded,
+                  size: Theme.of(context).appBarTheme.iconTheme!.size,
+                  color: Theme.of(context).appBarTheme.iconTheme!.color,
+                ),
+                onTap: () {
+                  debugPrint("Open Drawer");
+                  _scaffoldKey.currentState?.openDrawer();
+                },
+              ),
+              const SizedBox(width: 10),
+              Text(
+                "My Bin",
+                style: TextStyle(
+                  fontSize:
+                      Theme.of(context).textTheme.headlineMedium!.fontSize,
+                  fontWeight:
+                      Theme.of(context).textTheme.headlineMedium!.fontWeight,
+                  color: Theme.of(context).textTheme.headlineMedium!.color,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10)
+        ],
       ),
     );
   }
