9993c8037570c670bfa3e3f397d946959deb38b9
Chanathip <bipoeman12@gmail.com>
2024-02-23 23:17:26 +0700

    Add bit card assets image

--- lib/PinTheBin/home.dart
+++ lib/PinTheBin/home.dart
@@ -2,10 +2,10 @@ import "dart:convert";
 import "dart:developer";
 import "package:flutter/material.dart";
 import "package:flutter_map/flutter_map.dart";
+import "package:get/get.dart";
 import "package:latlong2/latlong.dart";
 import 'package:ruam_mitt/PinTheBin/bin_drawer.dart';
 import "package:ruam_mitt/PinTheBin/componant/map.dart";
-import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 
 import "package:http/http.dart" as http;
 import "package:ruam_mitt/PinTheBin/pin_the_bin_theme.dart";
