3cf4d78297e473ffb7383d243507a775a9a9f00f
Chanathip <bipoeman12@gmail.com>
2024-03-03 22:43:50 +0700

    change app logo to circle

--- lib/global_const.dart
+++ lib/global_const.dart
@@ -5,8 +5,8 @@ const String baseURL = "https://ruammitr.azurewebsites.net";
 // const String api = "https://ruammitr.azurewebsites.net/api";
 
 // const String api = "http://10.0.2.2:3000";
-const String api = "http://192.168.28.16:3000";
-// const String api = "http://10.0.2.2:3000";
+// const String api = "http://192.168.28.16:3000";
+const String api = "http://10.0.2.2:3000";
 // const String api = "http://192.168.56.1:3000";
 
 const String loginPageRoute = "/login";
