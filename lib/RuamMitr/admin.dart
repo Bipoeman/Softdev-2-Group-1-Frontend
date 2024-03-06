3113b1faef5e8f8cfafe2024a0f601519aee96d7
Bipoeman <75182220+Bipoeman@users.noreply.github.com>
2024-03-06 13:59:32 +0700

    pretty much no changes

--- lib/RuamMitr/admin.dart
+++ lib/RuamMitr/admin.dart
@@ -19,6 +19,7 @@ class AdminPage extends StatefulWidget {
 class ReportData {
   late Map<String, bool> isInitiallyBlank;
   late Map<String, TextEditingController> fieldController;
+  late Map<String, dynamic> userData;
   ReportData(List<String> fields) {
     isInitiallyBlank = {for (var element in fields) element: true};
     fieldController = {
@@ -39,6 +40,7 @@ class _AdminPageState extends State<AdminPage> {
       ReportData(["username", "title", "app", "description"]);
 
   bool isAccept = true;
+
 
   Future<void> getAllIssue() async {
     issueList = [];
@@ -81,6 +83,19 @@ class _AdminPageState extends State<AdminPage> {
       issueList.addAll(aceptedIssueList);
       issueList.addAll(rejectedIssueList);
     });
+    // for (int issueIndex = 0; issueIndex < issueList.length; issueIndex++) {
+    //   for (var eachUser in alluserData) {
+    //     print(issueList[issueIndex]['id']);
+    //     if (issueList[issueIndex]['id'] == eachUser['id']) {
+    //       issueList[issueIndex]['profilePicture'] = eachUser['profile'];
+    //       issueList[issueIndex]['fullname'] = eachUser['fullname'];
+    //       alluserData.remove(eachUser);
+    //     }
+    //   }
+    // }
+    // for( var issue in issueList){
+    //   print("issue $issue");
+    // }
     setState(() {});
   }
 
@@ -126,7 +141,7 @@ class _AdminPageState extends State<AdminPage> {
             onBoxOpen: () {
               // ["username", "title", "app", "description"]
               reportDataDisplay.fieldController['username']!.text =
-                  selectedIssue['user_id'].toString();
+                  selectedIssue['id'].toString();
               reportDataDisplay.fieldController['title']!.text =
                   selectedIssue['title'].toString();
               reportDataDisplay.fieldController['description']!.text =
