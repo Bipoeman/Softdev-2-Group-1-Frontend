10fdfdb1871de6958fc359e904bfdae699044ed8
SkyJTx <111367081+SkyJTx@users.noreply.github.com>
2024-03-09 00:20:18 +0700

    Move Settings to Client Settings

--- lib/RuamMitr/settings_v2.dart
+++ lib/RuamMitr/settings_v2.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_sliding_box/flutter_sliding_box.dart';
 import 'package:provider/provider.dart';
@@ -7,6 +6,7 @@ import 'package:ruam_mitt/RuamMitr/Component/avatar.dart';
 import 'package:ruam_mitt/RuamMitr/Component/theme.dart';
 import 'package:ruam_mitt/RuamMitr/admin.dart';
 import 'package:ruam_mitt/RuamMitr/Component/ruammitr_report.dart';
+import 'package:ruam_mitt/RuamMitr/Component/client_settings.dart';
 import 'package:ruam_mitt/global_const.dart';
 import 'package:ruam_mitt/global_var.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -20,39 +20,12 @@ class SettingsWidgetV2 extends StatefulWidget {
 }
 
 class _SettingsWidgetV2State extends State<SettingsWidgetV2> {
-  DropdownMenuItem<String> buildMenuItem(BuildContext context, String app) {
-    ThemeData theme = ThemesPortal.getCurrent(context).appThemes[app]!["light"]!.themeData;
-    CustomThemes currentTheme = ThemesPortal.appThemeFromContext(context, "RuamMitr")!;
-    return DropdownMenuItem(
-      value: app,
-      child: Row(
-        children: [
-          Container(
-            width: 25,
-            height: 25,
-            color: theme.colorScheme.primary,
-          ),
-          const SizedBox(
-            width: 5,
-          ),
-          Text(
-            app,
-            style: theme.textTheme.bodyLarge!.copyWith(
-              color: currentTheme.customColors["onContainer"],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
     Size size = MediaQuery.of(context).size;
     ThemeProvider themeProvider = Provider.of<ThemeProvider>(context);
     CustomThemes customTheme = ThemesPortal.appThemeFromContext(context, "RuamMitr")!;
-    List<String> appList = themeProvider.themeForApp.keys.toList();
 
     return SingleChildScrollView(
       physics: const RangeMaintainingScrollPhysics(),
@@ -78,153 +51,7 @@ class _SettingsWidgetV2State extends State<SettingsWidgetV2> {
                     ],
                   ),
                 ),
-                SizedBox(
-                  width: [size.width * 0.9, 300.0].reduce(min),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Container(
-                            color: theme.colorScheme.primary,
-                            width: size.height * 0.03,
-                            height: size.height * 0.03,
-                          ),
-                          SizedBox(width: size.width * 0.02),
-                          Text(
-                            "All Apps",
-                            style: TextStyle(
-                              fontSize: theme.textTheme.titleLarge!.fontSize,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(left: 30),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "Dark Mode",
-                              style: theme.textTheme.bodyLarge,
-                            ),
-                            Switch(
-                              value: themeProvider.isDarkMode,
-                              onChanged: (value) {
-                                themeProvider.toggleTheme();
-                              },
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  width: [size.width * 0.9, 300.0].reduce(min),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Container(
-                            color: ThemesPortal.getCurrent(context)
-                                .appThemes["RuamMitr"]!["light"]!
-                                .themeData
-                                .colorScheme
-                                .primary,
-                            width: size.height * 0.03,
-                            height: size.height * 0.03,
-                          ),
-                          SizedBox(width: size.width * 0.02),
-                          Text(
-                            "RuamMitr",
-                            style: TextStyle(
-                              fontSize: theme.textTheme.titleLarge!.fontSize,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(left: 30),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "Themes",
-                              style: theme.textTheme.bodyLarge,
-                            ),
-                            DropdownButton(
-                              dropdownColor: customTheme.customColors["container"]!,
-                              borderRadius: BorderRadius.circular(20),
-                              items: List.generate(appList.length, (index) {
-                                return buildMenuItem(context, appList[index]);
-                              }),
-                              value: ThemesPortal.getCurrent(context).themeForApp["RuamMitr"],
-                              onChanged: (value) {
-                                ThemesPortal.changeThemeColor(context, "RuamMitr", value!);
-                              },
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  width: [size.width * 0.9, 300.0].reduce(min),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Container(
-                            color: ThemesPortal.getCurrent(context)
-                                .appThemes["TuachuayDekhor"]!["light"]!
-                                .themeData
-                                .colorScheme
-                                .primary,
-                            width: size.height * 0.03,
-                            height: size.height * 0.03,
-                          ),
-                          SizedBox(width: size.width * 0.02),
-                          Text(
-                            "Dekhor",
-                            style: TextStyle(
-                              fontSize: theme.textTheme.titleLarge!.fontSize,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(left: 30),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "Themes",
-                              style: theme.textTheme.bodyLarge,
-                            ),
-                            DropdownButton(
-                              dropdownColor: customTheme.customColors["container"]!,
-                              borderRadius: BorderRadius.circular(20),
-                              items: List.generate(appList.length, (index) {
-                                return buildMenuItem(context, appList[index]);
-                              }),
-                              value: ThemesPortal.getCurrent(context).themeForApp["TuachuayDekhor"],
-                              onChanged: (value) {
-                                ThemesPortal.changeThemeColor(context, "TuachuayDekhor", value!);
-                              },
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+                const ClientSettingsWidget(),
                 // if (profileData['role'] == "User")
                 //   Padding(
                 //     padding:
