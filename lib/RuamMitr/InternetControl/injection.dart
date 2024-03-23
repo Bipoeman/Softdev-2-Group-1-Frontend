580bf5cca97d5a155045933bdd166809b4014432
SkyJTx <111367081+SkyJTx@users.noreply.github.com>
2024-03-23 23:49:45 +0700

    Remove other app

--- lib/RuamMitr/InternetControl/injection.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get/get.dart';
-import 'package:ruam_mitt/RuamMitr/InternetControl/network_controller.dart';
-
-class DependencyInjection {
-  static void init() {
-    Get.put<NetworkController>(NetworkController(), permanent: true);
-  }
-}
